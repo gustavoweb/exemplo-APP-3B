47690da76eaf48493849a0a9d3cd23a8621f2b70
gustavoweb <contato@gustavoweb.com>
2026-03-03 08:13:37 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+
+  return (
+    <>
+      <main>
+        <section>
+          <div id="menu">≡</div>
+          <div id="usuario">●</div>
+          <h1>Easy and quick Learn Language online!</h1>
+        </section>
+        <button>START</button>
+      </main>
+    </>
+  )
+}
+
